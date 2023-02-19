33fd46948df010bf021e40c2f2188d3e75f6100a
aira.calingasan@ffufm.com <108515469+ffuf-calingasan@users.noreply.github.com>
2023-02-19 21:24:14 +0800

    Add isArchived to activity and transaction entity

--- Domain/Activity.cs
+++ Domain/Activity.cs
@@ -1,12 +1,16 @@
+using Domain.Enums;
+
 namespace Domain
 {
-    public class Activity
-    {
-        public Guid Id { get; set; }
-        public Guid AccountId { get; set; }
-        public Account Account { get; set; } = default!;
-        public Guid TransactionId { get; set; }
-        public Transaction Transaction { get; set; } = default!;
-        public DateTime DateCreated { get; set; }
-    }
+  public class Activity
+  {
+    public Guid Id { get; set; }
+    public Guid AccountId { get; set; }
+    public Account Account { get; set; } = default!;
+    public Guid TransactionId { get; set; }
+    public Transaction Transaction { get; set; } = default!;
+    public ActivityType ActivityType { get; set; }
+    public bool IsArchived { get; set; }
+    public DateTime DateCreated { get; set; }
+  }
 }
--- Domain/Transaction.cs
+++ Domain/Transaction.cs
@@ -3,13 +3,14 @@ using Domain.Enums;
 
 namespace Domain
 {
-    public class Transaction
-    {
-        public Guid Id { get; set; }
-        public Guid AccountId { get; set; }
-        public Account Account { get; set; } = default!;
-        public TransactionType TransactionType { get; set; }
-        public double Amount { get; set; }
-        public DateTime Date { get; set; }
-    }
+  public class Transaction
+  {
+    public Guid Id { get; set; }
+    public Guid AccountId { get; set; }
+    public Account Account { get; set; } = default!;
+    public TransactionType TransactionType { get; set; }
+    public double Amount { get; set; }
+    public bool IsArchived { get; set; }
+    public DateTime Date { get; set; }
+  }
 }
